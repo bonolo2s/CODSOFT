4e95ef3f274873d3148314a339c654b2e4e887e2
Bonolo <bonololloyd003@gmail.com>
2024-02-08 17:51:13 +0200

    animated the about section

--- Portfolio website/main.js
+++ Portfolio website/main.js
@@ -6,3 +6,16 @@ var typed = new Typed(".roles",{
     loop: true
   })
   
+  /*gsap.to(".about-container", {
+    duration: 2, // animation will take 2 seconds
+    x: 0, // move the box 0px to the right
+    ease: "bounce", // use a bounce easing function
+    scrollTrigger: {
+      trigger: ".trigger",
+      start: "top bottom",
+      end: "+=500",
+      scrub: true
+    }
+  });*/
+  
+  
