4313229fa80f6c27a1da133b2fb1f8c2b01fe1ec
Bonolo <bonololloyd003@gmail.com>
2024-02-08 11:53:46 +0200

    initial commit

---
+++ Portfolio website/main.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".roles",{
+    strings:["Software Developer.","Email Developer."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  })
+  
+
+const menu = document.getElementById(menuIcon)
